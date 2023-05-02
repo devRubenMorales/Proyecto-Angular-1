109c013d02370224955b46193499f79eedd2064e
Ruben Morales <rm96090@gmail.com>
2023-05-02 16:32:05 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MiComponenteComponent } from './componentes/mi-componente/mi-componente.component';
+import { MiComponente2Component } from './componentes/mi-componente2/mi-componente2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MiComponenteComponent,
+    MiComponente2Component
   ],
   imports: [
     BrowserModule,
---
+++ src/app/componentes/mi-componente2/mi-componente2.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mi-componente2',
+  templateUrl: './mi-componente2.component.html',
+  styleUrls: ['./mi-componente2.component.css']
+})
+export class MiComponente2Component {
+
+}
